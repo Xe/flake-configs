1960ef29140b6bdfddd7a119fed34b9232ad41c3
Xe <me@christine.website>
2022-01-10 09:26:17 -0500

    add yggdrasil to logos
    Signed-off-by: Xe <me@christine.website>

--- hosts/logos/default.nix
+++ hosts/logos/default.nix
@@ -9,4 +9,18 @@
   networking.hostId = "aeace675";
 
   services.nginx.enable = true;
+
+  services.yggdrasil = {
+    enable = true;
+    persistentKeys = true;
+    openMulticastPort = true;
+    config = {
+      IfName = "yggdrasil0";
+      Peers = [
+        "tls://ca1.servers.devices.cwinfo.net:58226"
+        "tls://192.99.145.61:58226"
+        "tcp://kusoneko.moe:9002"
+      ];
+    };
+  };
 }
