c4f3430d3f272f88d91c331815406fe98bcf2121
Xe <me@christine.website>
2022-04-15 11:13:06 -0400

    fix copy error
    Signed-off-by: Xe <me@christine.website>

--- hardware/alrest/monitoring.nix
+++ hardware/alrest/monitoring.nix
@@ -1,16 +1,5 @@
 { config, pkgs, ... }:
 
-let metadata = pkgs.callPackage ../../../ops/metadata/peers.nix { };
-in {
-  services.prometheus = {
-    exporters = {
-      node = {
-        enable = true;
-        enabledCollectors = [ "systemd" ];
-      };
-      wireguard = {
-        enable = true;
-      };
-    };
-  };
+{
+  services.prometheus.exporters.node.enable = true;
 }
