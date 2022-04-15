589614cb6d7c02115e5b812ab18f88cd10c7256a
Xe <me@christine.website>
2022-04-15 11:24:49 -0400

    add pneuma
    Signed-off-by: Xe <me@christine.website>

---
+++ hosts/pneuma/default.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  virtualisation.docker.enable = true;
+  users.motd = builtins.readFile ./motd;
+  services.tailscale.port = 15430;
+
+  networking.hostName = "pneuma";
+  networking.hostId = "34fbd94b";
+}
