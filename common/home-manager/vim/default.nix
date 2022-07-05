b64411cb908297eb4e1418f96a2296c8eb9e3d69
Xe <me@christine.website>
2022-07-05 15:05:24 +0000

    fix vim
    Signed-off-by: Xe <me@christine.website>

--- common/home-manager/vim/default.nix
+++ common/home-manager/vim/default.nix
@@ -7,11 +7,7 @@ in {
   options.within.vim.enable = mkEnableOption "Enables Within's vim config";
 
   config = mkIf cfg.enable {
-    programs.vim = {
-      enable = true;
-      plugins = with pkgs.vimPlugins; [ vim-airline gruvbox vim-go direnv-vim vim-lsp ];
-    };
-
+    home.packages = [ pkgs.vim ];
     home.file.".vimrc".source = ./vimrc;
   };
 }
