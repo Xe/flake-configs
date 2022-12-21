1dd0ceece78c74979d67ed7d804ed7a917b38a7e
Xe <me@christine.website>
2022-12-21 13:04:41 -0500

    make wasmtime in pneuma's path
    Signed-off-by: Xe <me@christine.website>

--- hosts/pneuma/default.nix
+++ hosts/pneuma/default.nix
@@ -1,10 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" "wasm32-wasi" ];
   virtualisation.docker.enable = true;
   users.motd = builtins.readFile ./motd;
   services.tailscale.port = 15430;
+  environment.systemPackages = with pkgs; [ wasmtime ];
 
   networking.hostName = "pneuma";
   networking.hostId = "34fbd94b";
