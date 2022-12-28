8358d04641171d7994d2a1755224529d9b1599f8
Xe <me@christine.website>
2022-12-28 12:23:48 -0500

    add garnix cache
    Signed-off-by: Xe <me@christine.website>

--- common/default.nix
+++ common/default.nix
@@ -34,6 +34,7 @@
         "https://nix-community.cachix.org"
         "https://cuda-maintainers.cachix.org"
         "https://cache.floxdev.com?trusted=1"
+        "https://cache.garnix.io"
       ];
       trusted-users = [ "root" "cadey" ];
       trusted-public-keys = [
@@ -41,6 +42,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
   };
