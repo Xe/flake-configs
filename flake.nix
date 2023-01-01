e9856cb8d8734b4263e3f9c397ce9f46c7f91c2c
Xe <me@christine.website>
2023-01-01 14:28:23 -0500

    updates
    Signed-off-by: Xe <me@christine.website>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,11 @@
     home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
+
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     wsl = {
       url = "github:nix-community/NixOS-WSL";
