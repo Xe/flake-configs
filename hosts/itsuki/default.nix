939be50162bf94944e199158aa5517167cf7fbac
Xe Iaso <me@xeiaso.net>
2024-02-29 15:29:45 -0500

    hosts/itsuki: fuck you
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- hosts/itsuki/default.nix
+++ hosts/itsuki/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
@@ -24,7 +24,7 @@
 
   virtualisation.docker = {
     enable = true;
-    storageDriver = "zfs";
+    storageDriver = lib.mkForce "zfs";
   };
   systemd.services."docker" = {
     path = [ pkgs.zfs ];
